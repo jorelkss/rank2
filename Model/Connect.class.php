19d62c34dfdd9b295d8230e86ac484fab71a9aa8
JorelK <jpfl0101@alunos.ifce.edu.br>
2019-11-20 13:29:01 -0300

    Teste

--- Model/Connect.class.php
+++ Model/Connect.class.php
@@ -10,7 +10,14 @@
 		public static function get_instance(){
 			if(!isset(self::$instance)){
 				//Diz qual banco de dados vai se conectar
-				self::$instance = new PDO('mysql:host=localhost;dbname=bd_rank;', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+				$db = parse_url(getenv("postgres://ynpvaeudrjuuju:5d334a2bf1d4b4d1a15cbfee8bb17de75e565eb79290b621569788e75da3f17a@ec2-174-129-214-42.compute-1.amazonaws.com:5432/d24bjio0vnn90h"));
+				self::$instance = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")), array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 				self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 				self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);		
 			}
