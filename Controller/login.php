2dcd255a4a57099737496c807e62796cba3cc825
João Pedro <42539996+jorelkss@users.noreply.github.com>
2020-08-06 07:20:18 -0300

    Update login.php

--- Controller/login.php
+++ Controller/login.php
@@ -8,16 +8,15 @@
 	
 	$val = $_POST;
 	$data = new Manager();
-	$dados = $data->select("SELECT * FROM user_tb"); // Carrega os usuarios
-	
-	foreach ($dados as $dat) {
-		if($dat['email'] == $val['email']){ // Verifica se encontra um usuario com o email
-			if(password_verify($val['password'], $dat['password'])){ // Verifica se a senha está correta
-				$user = new User($dat['user_id'], $dat['nome'], $dat['email']);
-				$_SESSION['an_user'] = $user; // Coloca os dados do usuário da sessão
-				header("location: ".$GLOBALS['project_index']);
-			}
+	$dados = $data->select("SELECT * FROM user_tb WHERE email = '".$val['email']."'");
+
+	if($dados){
+		if(password_verify($val['password'], $dados[0]['password'])){
+			$user = new User($dados[0]['user_id'], $dados[0]['nome'], $dados[0]['email']);
+			$_SESSION['an_user'] = $user; // Coloca os dados do usuário da sessão
+			header("location: ".$GLOBALS['project_index']);
 		}
 	}
+	
 	header("location: ".$GLOBALS['project_index']);
 ?>
